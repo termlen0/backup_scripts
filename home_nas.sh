87c4bf6ab71926c7e9b54b797814018b2404c4fd
Ajay Chenampara <ajay.chenampara@gmail.com>
2017-03-17 16:23:18 -0400

    fix IP

--- home_nas.sh
+++ home_nas.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-ping -c5 -q 192.168.1.122
+ping -c5 -q 192.168.1.103
 if [ $? -eq 0 ]
+echo "=== Starting backup ==="
 then /home/ajay/backup_scripts/backup.sh
+echo "=== Backup done `date`==="
      exit 0
 else echo “`date` - Backup failed”
 fi
